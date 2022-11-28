736f334e3842e2cc3302ecbe1e9b96343260c87f
bombies <xjamesdzn@gmail.com>
2022-11-28 13:55:18 -0500

    Add colour

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,20 +6,13 @@ export default function Home() {
 
   return (
       <Layout>
-        <div className='h-full pt-16'>
-            <p className='text-center font-bold text-6xl mb-6'>Calcify!</p>
+        <div className='h-full pt-16 pink-gradient'>
+            <p className='text-center font-bold text-8xl mb-6 text-white drop-shadow-lg'>Calcify!</p>
             {/*Calculator*/}
-            <div className='h-3/4 w-1/2 bg-neutral-700 mx-auto rounded-lg'>
-                <div className='h-32 rounded-lg bg-black'></div>
+            <div className='h-3/4 w-1/2 bg-neutral-200 mx-auto rounded-lg'>
+                <div className='h-32 rounded-lg bg-pink-300'></div>
                 <div className='grid grid-cols-5 py-6 gap-y-8'>
-                    <p className='text-center'>1</p>
-                    <p className='text-center'>1</p>
-                    <p className='text-center'>1</p>
-                    <p className='text-center'>1</p>
-                    <p className='text-center'>1</p>
-                    <p className='text-center'>1</p>
-                    <p className='text-center'>1</p>
-                    <p className='text-center'>1</p>
+
                 </div>
             </div>
         </div>
